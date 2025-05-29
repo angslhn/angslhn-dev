f02c9a78d3ac3b3b57a43cb3a27166721e358557
Aang Solihin <angslhn@gmail.com>
2025-05-29 20:37:35 +0700

    angslhn-dev v0.0.1 - Setup awal web portofolio.

---
+++ src/app/layout.tsx
@@ -0,0 +1,42 @@
+import type { Metadata } from "next";
+import "@/styles/globals.css"
+
+export const metadata: Metadata = {
+    title: 'Aang Solihin | Dev Profile',
+    authors: [{ name: "Aang Solihin", url: "https://angslhn.pages.dev" }],
+    creator: "Aang Solihin",
+    keywords: ["Aang Solihin", "Frontend Developer", "Backend Developer", "Fullstack Developer", "Informatics Engineering", "AI", "Machine Learning", "ReactJS", "NextJS", "Python", "Tailwindcss", "Web Developer", "Student"],
+    description: "On this portfolio site Aang Solihin displays several things and information about himself. Be it a brief description of himself, his expertise, his experience, and his contact information.",
+    generator: "Next.js",
+    robots: {
+      index: true,
+      follow: true
+    },
+    alternates: {
+      canonical: "https://angslhn.pages.dev",
+    },
+    icons: {
+        icon: [
+          { url: '/favicon.svg', type: 'image/svg+xml' },
+          { url: '/favicon.ico', sizes: '32x32' },
+        ],
+        shortcut: '/favicon.ico',
+        apple: [
+          { url: '/apple-favicon.png', sizes: '180x180' },
+        ]
+    },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
